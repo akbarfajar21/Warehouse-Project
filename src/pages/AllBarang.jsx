ea97e8f48150795c5e3b03b54b770e9293886227
akbarfajar21 <akbarfajar2112@gmail.com>
2024-09-26 09:30:12 +0700

    versi 1.3

--- src/pages/AllBarang.jsx
+++ src/pages/AllBarang.jsx
@@ -9,8 +9,8 @@ const Modal = ({ isOpen, onClose, item, filter }) => {
   if (!isOpen || !item) return null; 
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-md shadow-lg w-96">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white p-6 rounded-md shadow-lg w-96 z-50">
         {filter === "barang" ? (
           <>
             <h3 className="text-xl font-semibold mb-4">{item.nama_barang}</h3>
